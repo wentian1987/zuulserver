163398b7491c32d42cbe12b1edafaf73096617d1
wentian <wentian1@126.com>
2017-04-16 22:05:44 +0800

    init wentian

---
+++ src/main/java/com/picc/zuulserver/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.picc.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by huangchong05 on 2016/11/29.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
